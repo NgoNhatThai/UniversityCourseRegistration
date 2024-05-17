703532f2684041d45276b1cd5f6e299ad4303371
NgoNhatThai <ngothai9a1@gmail.com>
2024-05-18 00:08:01 +0700

    add api get schedules

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -14,6 +14,7 @@ const IntRoutesStudent = (router) => {
         .get(studentController.getStudentStatus);
     router.route('/getStatus')
         .get(studentController.getStatus);
+
     return router;
 }
 
